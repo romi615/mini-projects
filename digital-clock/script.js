362615347b2e4d1cf034e7f6198295d821140fc4
romi615 <romimawandia615@gmail.com>
2026-01-01 13:52:06 +0530

    comment out for better understanding

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -1,10 +1,12 @@
+// declare variables and store their referrences inside variables
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 
 
+// function to digital clock
 function digitalClock(){
-    
+
 const date = new Date();
 
 const h = date.getHours();
@@ -17,4 +19,6 @@ const s = date.getSeconds();
  seconds.innerText = s < 10 ? "0" + s : s;
 
 }
+
+// setInterval function update time every second
 setInterval(digitalClock, 1000);
