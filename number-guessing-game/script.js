62ceb1a93ce46fb6d8d7640c3b3d4e7a9d893e2a
romi615 <romimawandia615@gmail.com>
2026-01-02 10:24:18 +0530

    JS: add user guess functionality

---
+++ number-guessing-game/script.js
@@ -0,0 +1,24 @@
+const userInput = document.getElementById("userInput")
+const guessBtn = document.getElementById("guessBtn")
+const feedbackBox = document.getElementById("feedbackBox")
+
+
+function userGuess(){
+    const randomNumber = Math.round(Math.random()*10)
+    // console.log(randomNumber);
+
+    const input = Number(userInput.value);
+    // console.log(input)
+
+    if(input > randomNumber){
+        feedbackBox.innerHTML = "high";
+    } else if(input < randomNumber){
+        feedbackBox.innerText = 'low';
+    } else {
+        feedbackBox.innerText = "correct"
+    }
+    userInput.value = ""
+    
+}
+
+guessBtn.addEventListener('click', userGuess);
