1028ca281118aa1cf57f6d5c4368bd8fc8c0580d
romi615 <romimawandia615@gmail.com>
2026-01-02 12:20:43 +0530

    add mark task completed functionality

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -2,28 +2,36 @@ const userInput = document.getElementById("userInput")
 const addTaskBtn = document.getElementById("addTaskBtn")
 const todoList = document.getElementById("todo-list")
 
+// function markTaskCompleted(){
 
+// }
 
 function removeTask(event){
     // console.log(event.target.parentElement);
-    const target = event.target
-    if(target.tagName === "BUTTON"){
-        target.parentElement.remove();
     }
-}
 
 function addTask(){
 
     const list = document.createElement('li')
-    list.innerText = userInput.value;
+
+    const input = document.createElement('input')
+    input.setAttribute('type', 'checkbox');
+    input.classList.add('task-completed')
+    list.appendChild(input);
+
+    const span = document.createElement('span')
+    span.innerText = userInput.value;
+    // span.classList.add('task-text')
+    list.appendChild(span)
 
     const button = document.createElement('button')
     button.innerText = "Remove";
+    button.classList.add('task-btn')
     list.appendChild(button);
 
-    button.addEventListener('click', (e)=>{
-        removeTask(e);
-    });
+    // button.addEventListener('click', (e)=>{
+    //     removeTask(e);
+    // });
 
     todoList.appendChild(list);
 
@@ -33,3 +41,19 @@ function addTask(){
 
 
 addTaskBtn.addEventListener('click', addTask);
+
+
+todoList.addEventListener('click', (event)=>{
+
+if(event.target.classList.contains('task-btn')){
+    event.target.parentElement.remove();
+    return;
+}
+
+if(event.target.classList.contains("task-completed"))
+{
+    event.target.nextElementSibling.classList.toggle('task-text');
+}
+    
+})
+
