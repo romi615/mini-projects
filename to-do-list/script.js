502c3aba5f1ff3815ab6be51f0d259798808f15d
romi615 <romimawandia615@gmail.com>
2026-01-02 11:43:25 +0530

    add remove task functionality

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -2,14 +2,33 @@ const userInput = document.getElementById("userInput")
 const addTaskBtn = document.getElementById("addTaskBtn")
 const todoList = document.getElementById("todo-list")
 
+
+
+function removeTask(event){
+    // console.log(event.target.parentElement);
+    const target = event.target
+    if(target.tagName === "BUTTON"){
+        target.parentElement.remove();
+    }
+}
+
 function addTask(){
 
     const list = document.createElement('li')
     list.innerText = userInput.value;
 
+    const button = document.createElement('button')
+    button.innerText = "Remove";
+    list.appendChild(button);
+
+    button.addEventListener('click', (e)=>{
+        removeTask(e);
+    });
+
     todoList.appendChild(list);
 
 }
+
 
 
 
