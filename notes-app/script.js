a6bd930d95a0bb0e75eb6765818f600270c2cdcb
romi615 <romimawandia615@gmail.com>
2026-01-03 12:13:31 +0530

    add render function

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,6 +1,7 @@
 const title = document.getElementById("notesTitle")
 const content = document.getElementById("notesContent")
 const notesBtn = document.getElementById("notesBtn")
+const notesList = document.querySelector(".notes-list")
 
 function setNotes(notes){
     localStorage.setItem('notes', JSON.stringify(notes));
@@ -9,6 +10,22 @@ function setNotes(notes){
 function getNotes(){
     const data = localStorage.getItem("notes")
     return data ? JSON.parse(data) : []
+}
+
+function renderNotes(){
+    const notes = getNotes();
+
+    notesList.innerHTML = "";
+
+    notes.forEach((note)=>{
+            const list = document.createElement('li');
+
+            list.innerHTML = `<h3>${note.title}</h3>
+                <p>${note.content}</p>`
+
+
+                notesList.appendChild(list);
+    })
 }
 
 function addNotes(title, content){
@@ -24,6 +41,8 @@ function addNotes(title, content){
 
     notes.push(newNotes);
     setNotes(notes);
+
+    renderNotes();
 
 
 }
