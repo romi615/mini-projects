5518af1274c9f9fdc50a6108f6651f3e8a950445
romi615 <romimawandia615@gmail.com>
2026-01-03 16:57:15 +0530

    every

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -2,58 +2,65 @@ const questions = [
   {
     question: "Who is the prime minister of india in 2014?",
     answer: [
-      { option: "Indira Gandhi", isCorrect : false},
-      { option: "Rajiv Gandhi" ,isCorrect : false},
-      { option: "Manmohan Singh" ,isCorrect : false},
-      { option: "Narendra Modi" ,isCorrect : true},
+      { option: "Indira Gandhi", isCorrect: false },
+      { option: "Rajiv Gandhi", isCorrect: false },
+      { option: "Manmohan Singh", isCorrect: false },
+      { option: "Narendra Modi", isCorrect: true },
     ],
   },
 
   {
     question: "Who painted the Mona Lisa?",
     answer: [
-      { option: "Vincent van Gogh", isCorrect : false},
-      { option: "Pablo Picasso" ,isCorrect : false},
-      { option: "Leonardo da Vinci" ,isCorrect : true},
-      { option: "Claude Monet" ,isCorrect : false},
+      { option: "Vincent van Gogh", isCorrect: false },
+      { option: "Pablo Picasso", isCorrect: false },
+      { option: "Leonardo da Vinci", isCorrect: true },
+      { option: "Claude Monet", isCorrect: false },
     ],
   },
 
   {
     question: "What is the capital of Japan?",
     answer: [
-      { option: "Beijing", isCorrect : false},
-      { option: "Tokyo" ,isCorrect : true},
-      { option: "Seoul" ,isCorrect : false},
-      { option: "Bangkok" ,isCorrect : false},
+      { option: "Beijing", isCorrect: false },
+      { option: "Tokyo", isCorrect: true },
+      { option: "Seoul", isCorrect: false },
+      { option: "Bangkok", isCorrect: false },
     ],
   },
 ];
 
-const question = document.getElementById('question');
+const question = document.getElementById("question");
 const options = document.querySelectorAll(".option");
+const nextBtn = document.getElementById("nextBtn");
 
 let questionCount = 0;
 
-function showQuestion(){
+function showQuestion() {
+  let answerCount = 0;
 
-    let answerCount = 0;
-
+  if (questionCount < questions.length) {
     question.innerText = questions[questionCount].question;
 
-    options.forEach(option=>{
-        option.innerText = questions[questionCount].answer[answerCount++].option;
-        // console.log(option)
-        option.addEventListener('click', (e)=>{
-            console.log(e.target);
-        })
-    })
+    options.forEach((option) => {
+      option.innerText = questions[questionCount].answer[answerCount].option;
+      option.dataset.id = questions[questionCount].answer[answerCount].isCorrect;
+      answerCount++;
+    });
 
+    
     questionCount++;
+  } else {
+    startQuiz();
+  }
 }
 
-
-
-showQuestion();
+nextBtn.addEventListener("click", () => {
+  showQuestion();
+});
 // showQuestion();
 // showQuestion();
+
+function startQuiz() {
+  questionCount = 0;
+}
