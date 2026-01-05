0be50320518bfdfb9eb28a3b4513441d620928eb
romi615 <romimawandia615@gmail.com>
2026-01-05 12:57:24 +0530

    add local storage

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,52 +1,73 @@
-const userInput = document.getElementById("userInput")
-const addTaskBtn = document.getElementById("addTaskBtn")
-const todoList = document.getElementById("todo-list")
+const userInput = document.getElementById("userInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const todoList = document.getElementById("todo-list");
 
+function getTask() {
+  const data = localStorage.getItem("task");
+  return data ? JSON.parse(data) : [];
+}
 
+function setTask(task) {
+  localStorage.setItem("task", JSON.stringify(task));
+}
 
-function addTask(){
+function removeTask(id) {
+  let taskList = getTask();
+  taskList = taskList.filter((task) => task.id !== id);
+  setTask(taskList);
+  renderTask();
+}
 
-    const list = document.createElement('li')
+function renderTask() {
+  const taskList = getTask();
+  todoList.innerHTML = "";
 
-    const input = document.createElement('input')
-    input.setAttribute('type', 'checkbox');
-    input.classList.add('task-completed')
+  taskList.forEach((task) => {
+    const list = document.createElement("li");
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "checkbox");
+    input.classList.add("task-completed");
     list.appendChild(input);
 
-    const span = document.createElement('span')
-    span.innerText = userInput.value;
-    list.appendChild(span)
+    const span = document.createElement("span");
+    span.innerText = task.task;
+    list.appendChild(span);
 
-    const button = document.createElement('button')
+    const button = document.createElement("button");
     button.innerText = "Remove";
-    button.classList.add('task-btn')
+    button.dataset.id = task.id;
     list.appendChild(button);
-
-    
 
     todoList.appendChild(list);
 
     userInput.value = "";
-
+  });
 }
 
+function addTask(task) {
+  let taskList = getTask();
 
+  const newList = {
+    id: Date.now(),
+    task: task,
+  };
 
+  console.log(newList);
 
-addTaskBtn.addEventListener('click', addTask);
-
-
-todoList.addEventListener('click', (event)=>{
-
-if(event.target.classList.contains('task-btn')){
-    event.target.parentElement.remove();
-    return;
+  taskList.push(newList);
+  setTask(taskList);
+  renderTask();
 }
 
-if(event.target.classList.contains("task-completed"))
-{
-    event.target.nextElementSibling.classList.toggle('task-text');
-}
-    
-})
+addTaskBtn.addEventListener("click", () => {
+  addTask(userInput.value);
+});
 
+todoList.addEventListener("click", (event) => {
+  removeTask(Number(event.target.dataset.id));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderTask();
+});
