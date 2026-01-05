c3a656efbb5db91aaadfc0a6f21d90e369dc7fcd
romi615 <romimawandia615@gmail.com>
2026-01-05 11:49:15 +0530

    add local storage

--- color-changer/script.js
+++ color-changer/script.js
@@ -1,6 +1,12 @@
 // declare a variable and store its     referrence inside declare variable
 const colorBtn = document.getElementById("colorBtn");
 
+
+
+function saveColorChanger(color){
+  localStorage.setItem('color', color);
+}
+
 //    function to color change
 function colorChanger() {
   const hexValue = "0123456789ABCDEF";
@@ -8,10 +14,17 @@ function colorChanger() {
 
   for (let i = 0; i < 6; i++) {
     color += hexValue[Math.floor(Math.random() * hexValue.length)];
-
-    document.body.style.backgroundColor = color;
   }
+
+  saveColorChanger(color);
+
+  document.body.style.backgroundColor = localStorage.getItem('color');
+
 }
 
 // attach event handler with button (colorBtn)
 colorBtn.addEventListener("click", colorChanger);
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.body.style.backgroundColor = localStorage.getItem('color');
+})
