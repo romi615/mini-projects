ee30e2c474a3095405365bb779385b5011a6ea31
romi615 <romimawandia615@gmail.com>
2026-01-05 11:29:21 +0530

    add foramtting

--- counter-app/script.js
+++ counter-app/script.js
@@ -1,32 +1,45 @@
 // declare variables and take elements' referrences and assign them to variables
-const count = document.getElementById('count')
-const incrementBtn = document.getElementById('incrementBtn')
-const resetBtn = document.getElementById('resetBtn')
-const decrementBtn = document.getElementById('decrementBtn')
+const count = document.getElementById("count");
+const incrementBtn = document.getElementById("incrementBtn");
+const resetBtn = document.getElementById("resetBtn");
+const decrementBtn = document.getElementById("decrementBtn");
 
 // declare a variable storeCount to store counting
-let storeCount = 0;
+let storeCount = localStorage.getItem("storeCount")
+  ? Number(localStorage.getItem("storeCount"))
+  : 0;
+
+  // intial value
+count.innerText = storeCount;
+
+function saveCount() {
+  localStorage.setItem("storeCount", storeCount);
+}
 
 // function to increment
-function increment(){
-    count.innerText = ++storeCount;
+function increment() {
+  storeCount++;
+  count.innerText = storeCount;
+  saveCount();
 }
 
 // function to decrement
-function decrement(){
-    if(storeCount > 0){
-        count.innerText = --storeCount;
-    }
+function decrement() {
+  if (storeCount > 0) {
+    storeCount--;
+    count.innerText = storeCount;
+  }
+  saveCount();
 }
 
 // function to reset
-function reset(){
-    storeCount = 0;
-    count.innerText = storeCount;
+function reset() {
+  storeCount = 0;
+  count.innerText = storeCount;
+  saveCount();
 }
 
-
 // attach event handlers with buttons(increment, reset, and decrement)
-incrementBtn.addEventListener('click',increment);
-resetBtn.addEventListener('click',reset);
-decrementBtn.addEventListener('click',decrement);
+incrementBtn.addEventListener("click", increment);
+resetBtn.addEventListener("click", reset);
+decrementBtn.addEventListener("click", decrement);
