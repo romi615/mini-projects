70b756625f6bda7f17c86996de3a1a1589b95ab5
romi615 <romimawandia615@gmail.com>
2026-01-06 11:55:16 +0530

    add random quote generator logic

---
+++ random-quote-generator/script.js
@@ -0,0 +1,13 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author-name");
+
+
+async function randomQuoteGenerator() {
+  const response = await fetch("https://api.quotable.io/quotes/random?maxLength=50");
+  const data = await response.json();
+  console.log(data);
+  quote.innerText = data[0].content;
+  author.innerText = data[0].author;
+}
+
+randomQuoteGenerator();
