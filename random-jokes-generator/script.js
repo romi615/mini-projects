509c93cae0e2f1018a4f5f09b5cff77b44a14ddb
romi615 <romimawandia615@gmail.com>
2026-01-11 11:40:43 +0530

    JS: add basic logic

---
+++ random-jokes-generator/script.js
@@ -0,0 +1,8 @@
+const joke = document.getElementById('Joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+const url = "https://official-joke-api.appspot.com/random_joke"
+
+async function jokeGenerator(){
+    
+}
