e889f86fe6a048277865ca38a72744f0d9b89451
romi615 <romimawandia615@gmail.com>
2026-01-11 11:57:40 +0530

    add joke generator logic

--- random-jokes-generator/script.js
+++ random-jokes-generator/script.js
@@ -1,8 +1,19 @@
-const joke = document.getElementById('Joke');
-const jokeBtn = document.getElementById('jokeBtn');
+const questionJoke = document.querySelector('.questionJoke');
+const answerJoke = document.querySelector('.answerJoke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
 
 const url = "https://official-joke-api.appspot.com/random_joke"
 
 async function jokeGenerator(){
-    
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+
+questionJoke.innerHTML=`<span>Question: </span>${data.setup}`
+answerJoke.innerHTML=`<span>Answer: </span>${data.punchline}`
 }
+
+jokeBtn.addEventListener('click',()=>{
+    jokeGenerator();
+})
