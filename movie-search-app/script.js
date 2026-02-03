190f8b38ccf4713f319fd29287dc59c3ce133e0d
romi615 <romimawandia615@gmail.com>
2026-02-03 17:33:04 +0530

    Initial ugly draft: movie search working with API

---
+++ movie-search-app/script.js
@@ -0,0 +1,19 @@
+const button = document.getElementById("searchBtn");
+const input = document.getElementById("searchInput");
+const container = document.getElementById("movieContainer");
+
+button.addEventListener("click", function () {
+  const movieName = input.value;
+
+  fetch(`https://www.omdbapi.com/?apikey=YOUR_API_KEY&s=${movieName}`)
+    .then(response => response.json())
+    .then(data => {
+      container.innerHTML = "";
+
+      data.Search.forEach(movie => {
+        const p = document.createElement("p");
+        p.innerText = movie.Title;
+        container.appendChild(p);
+      });
+    });
+});
